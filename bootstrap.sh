abb074e7855732c43877d83e6484d3538ec8a608
Márk Gerencsér <anonymus5000@gmail.com>
2023-03-30 16:29:00 +0200

    bootstrap.sh changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,15 @@ apt-get update
 chpasswd <<< "root:Alma1234"
 apt-get install mc -y
 apt-get install htop -y
+apt-get install openjdk-11-jdk -y
+
+useradd -m -d /opt/udemx udemx
+chpasswd <<< "udemx:udemx"
+
+apt-get install openssh-server -y
+systemctl status ssh
+nano /etc/ssh/sshd_confix
+^\ 
+#ssh debian@0.0.0.0
 
 #apt-get install -y apache2
